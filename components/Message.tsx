db009cd3c56d7bc2eb759296e45658f402e6b06e
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-16 14:23:52 -0500

    feat: add components

---
+++ components/Message.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import { IMessageProps } from '../interfaces'
+
+export const Message = ({
+  children,
+  avatar,
+  username,
+  description
+}: IMessageProps) => {
+  return (
+    <div className='bg-white p-8 border-b-2 rounded-lg'>
+      <div className='flex items-center gap-2'>
+        <Image
+          src={avatar || 'https://i.pravatar.cc/150?img=32'}
+          alt='illustration-1'
+          className='w-10 rounded-full'
+          width={50}
+          height={50}
+        />
+        <h2 className='font-bold'>{username}</h2>
+      </div>
+      <div className='py-4'>
+        <p title={description} className='truncate py-2 font-medium'>
+          {description}
+        </p>
+      </div>
+      {children}
+    </div>
+  )
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../utils/firebase'
+
+export const Nav = () => {
+  const [user, loading] = useAuthState(auth)
+  return (
+    <>
+      <nav className='flex justify-between items-center py-10'>
+        <Link href='/'>
+          <button className='text-lg font-medium'>Creative Minds</button>
+        </Link>
+        <ul className='flex items-center gap-10'>
+          {!user && (
+            <Link href='/auth/login'>
+              <a className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8 cursor-pointer'>
+                Join Now
+              </a>
+            </Link>
+          )}
+          {user && (
+            <div className='flex items-center gap-6'>
+              <Link href='/post'>
+                <button className='font-medium bg-cyan-500 text-white py-2 px-4 rounded-md text-sm'>
+                  Post
+                </button>
+              </Link>
+              <Link href='/dashboard'>
+                <a className='m-auto flex'>
+                  <Image
+                    className='w-12 rounded-full cursor-pointer'
+                    src={user.photoURL || 'https://i.pravatar.cc/150?img=32'}
+                    alt=''
+                    width={40}
+                    height={40}
+                    objectFit='cover'
+                  />
+                </a>
+              </Link>
+            </div>
+          )}
+        </ul>
+      </nav>
+    </>
+  )
+}
