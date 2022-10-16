4bbfbab484f0fb03998ccaecb5181954db04f308
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-16 14:23:44 -0500

    feat: add pages

---
+++ pages/dashboard.tsx
@@ -0,0 +1,99 @@
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where
+} from 'firebase/firestore'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { AiFillEdit } from 'react-icons/ai'
+import { BsTrash2Fill } from 'react-icons/bs'
+import { toast } from 'react-toastify'
+import { Loader } from '../components/Loader'
+import { Message } from '../components/Message'
+import { auth, db } from '../utils/firebase'
+
+const Dashboard = () => {
+  const route = useRouter()
+  const [user, loading] = useAuthState(auth)
+  const [posts, setPosts] = useState([])
+  const [loadingPosts, setLoadingPosts] = useState(false)
+  // Delete post
+  const deletePost = async (id: string) => {
+    try {
+      const docRef = doc(db, 'posts', id)
+      await deleteDoc(docRef)
+    } catch (error) {
+      console.error(error)
+      toast.error('Something went wrong', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 1500
+      })
+    }
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoadingPosts(true)
+        if (loading) return <h1>Loading...</h1>
+        if (!user) route.push('/auth/login')
+        const collectionsRef = collection(db, 'posts')
+        const queryUser = query(collectionsRef, where('user', '==', user?.uid))
+        const unsubscribe = onSnapshot(queryUser, (snapshot: any) => {
+          setPosts(
+            snapshot.docs.map((doc: any) => ({ id: doc.id, ...doc.data() }))
+          )
+          setLoadingPosts(false)
+        })
+        return unsubscribe
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getData()
+  }, [user, loading])
+
+  return (
+    <div>
+      <h1>Your posts</h1>
+      <div>
+        {loadingPosts ? (
+          <Loader />
+        ) : (
+          posts.map((post: any) => (
+            <Message key={post.id} {...post}>
+              <div className='flex gap-4'>
+                <button
+                  className='text-pink-600 flex items-center justify-center gap-2 py-2 text-sm'
+                  onClick={() => deletePost(post.id)}
+                >
+                  <BsTrash2Fill className='text-2xl' />
+                  Delete
+                </button>
+                <Link href={{ pathname: '/post', query: post }}>
+                  <button className='text-teal-600 flex items-center justify-center gap-2 py-2 text-sm'>
+                    <AiFillEdit className='text-2xl' />
+                    Edit
+                  </button>
+                </Link>
+              </div>
+            </Message>
+          ))
+        )}
+      </div>
+      <button
+        className='font-medium text-white bg-gray-800 py-2 px-4 my-6 '
+        onClick={() => auth.signOut()}
+      >
+        Sign out
+      </button>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ pages/post.tsx
@@ -0,0 +1,119 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc
+} from 'firebase/firestore'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { toast } from 'react-toastify'
+import { auth, db } from '../utils/firebase'
+
+export interface IPostData {
+  description: string
+  id?: string
+  title: string
+}
+
+const Post = () => {
+  const [post, setPost] = useState<IPostData>({ description: '', title: '' })
+  const [isDisabled, setIsDisabled] = useState(false)
+  const [user, loading] = useAuthState(auth)
+  const route = useRouter()
+  const routeData = route.query
+
+  const submitPost = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      setIsDisabled(true)
+      if (!post.description) {
+        return toast.error('Description is required  😅', {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 1500
+        })
+      }
+
+      const collectionRef = collection(db, 'posts')
+      if (post.id) {
+        const docRef = doc(db, 'posts', post.id)
+        await updateDoc(docRef, {
+          ...post,
+          timestamp: serverTimestamp()
+        })
+        route.push('/dashboard')
+      } else {
+        await addDoc(collectionRef, {
+          ...post,
+          timestamp: serverTimestamp(),
+          user: user?.uid,
+          avatar: user?.photoURL,
+          username: user?.displayName
+        })
+        setPost({ description: '', title: '' })
+        return route.push('/')
+      }
+    } catch (error) {
+      console.error(error)
+      toast.error('Something went wrong')
+    } finally {
+      setIsDisabled(false)
+    }
+  }
+
+  useEffect(() => {
+    const checkUser = async () => {
+      if (loading) return <h1>Loading...</h1>
+      if (!user) route.push('/auth/login')
+      if (routeData?.id) {
+        setPost({
+          description: routeData?.description as string,
+          id: routeData?.id as string,
+          title: routeData?.title as string
+        })
+      }
+    }
+    checkUser()
+  }, [loading, user])
+
+  return (
+    <div className='my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto'>
+      <form onSubmit={submitPost}>
+        <h1 className='text-2xl font-bold'>
+          {post.id ? 'Edit your Post' : 'Create a new Post'}
+        </h1>
+        <div className='py-2'>
+          <h3 className='text-lg font-medium py-2'>Description</h3>
+          <textarea
+            className='bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm'
+            name=''
+            id=''
+            cols={1}
+            rows={1}
+            maxLength={300}
+            value={post.description}
+            onChange={(e) => setPost({ ...post, description: e.target.value })}
+          ></textarea>
+          <p
+            className={`text-cyan-600 font-medium text-sm ${
+              post.description.length > 300 ? 'text-red-600' : ''
+            }`}
+          >
+            {post.description.length}/300
+          </p>
+        </div>
+        <button
+          className='w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm disabled:opacity-75'
+          type='submit'
+          disabled={isDisabled}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Post
