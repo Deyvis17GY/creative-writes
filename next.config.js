0b992c736782c27333ffaf03cd1409fe9bad5fbf
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-16 14:21:37 -0500

    feat: add config tailwind css

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.pravatar.cc',
+      'lh3.googleusercontent.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
