6daa729c9ddbe3313d7230b512abc179c940259f
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-18 10:28:05 -0500

    feat: add config images google

--- components/Message.tsx
+++ components/Message.tsx
@@ -16,11 +16,22 @@ export const Message = ({
           className='w-10 rounded-full'
           width={50}
           height={50}
+          referrerPolicy='no-referrer'
         />
+
+        {/* <img
+          src={avatar || 'https://i.pravatar.cc/150?img=32'}
+          alt='illustration-1'
+          className='w-10 rounded-full'
+          width={50}
+          height={50}
+          referrerPolicy='no-referrer'
+        /> */}
+
         <h2 className='font-bold'>{username}</h2>
       </div>
       <div className='py-4'>
-        <p title={description} className='truncate py-2 font-medium'>
+        <p title={description} className='py-2 font-medium'>
           {description}
         </p>
       </div>
