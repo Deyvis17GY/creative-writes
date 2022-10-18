2e90f9541cbdab729e8c1ccaafc0d96df6ffde26
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-18 10:27:43 -0500

    🔧 : add config to pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
+
 const nextConfig = {
+  assetPrefix: isDevelopment ? 'creative-writes' : '',
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: [
+      'lh3.googleusercontent.com',
       'images.unsplash.com',
-      'i.pravatar.cc',
-      'lh3.googleusercontent.com'
+      'i.pravatar.cc'
     ]
   }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
