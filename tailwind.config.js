8d8e34e1726b92bae15ca1890cd1bb2b0db5b836
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-19 20:17:30 -0500

    🔧 : configure responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ module.exports = {
       fontFamily: {
         poppins: 'Poppins'
       }
+    },
+    screens: {
+      phone: {
+        max: '640px'
+      },
+      sm: {
+        min: '640px'
+      },
+      md: {
+        min: '768px'
+      },
+      lg: {
+        min: '1024px'
+      },
+      xl: {
+        min: '1280px'
+      }
     }
   },
   plugins: []
