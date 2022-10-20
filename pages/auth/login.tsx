bbf8ba478582f590434bcb325be73aab6778da8d
Deyvis Mariños <deyvisjaime17@gmail.com>
2022-10-19 21:33:03 -0500

    feat: add theme dark/light to login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -33,9 +33,11 @@ const Login = () => {
 
   return (
     <div className='shadow-xl mt-32 p-10 text-gray-700  rounded-lg'>
-      <h2 className='text-2xl font-medium'>{t('login.subtitle')}</h2>
+      <h2 className='text-2xl font-medium dark:text-white'>
+        {t('login.subtitle')}
+      </h2>
       <div className='py-4'>
-        <h3 className='py-4'>{t('login.providers')}</h3>
+        <h3 className='py-4 dark:text-white'>{t('login.providers')}</h3>
         <button
           className='text-white bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4 gap-2'
           onClick={googleLogin}
